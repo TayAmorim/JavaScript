75f69e7525a34a85e075cc75107cded6c3a6d18b
TayAmorim <amorim.tayanna@gmail.com>
2022-06-05 15:11:47 -0300

    Solução 2 da atividade prática polídromo

--- palindromo/palindromo.js
+++ palindromo/palindromo.js
@@ -1,25 +1,38 @@
 //Solução 1
-function verificaPalindromo(string) {
+/*function verificaPalindromo(string) {
   if (!string) return
 
   return string.split('').reverse().join('') === string
 }
 
-console.log(verificaPalindromo("gato"))
+console.log(verificaPalindromo('gato'))
 
-// Solução modificada pessoalmente
+// Solução modificada pela usúaria
 
 let nome = prompt('Digite o nome que você deseja saber se é um palíndromo: ')
 
-function verificaPalindromo() {
-
+function verificaPalindromo1() {
   if (nome.split('').reverse().join('') === nome) {
     alert('O nome que você digitou é um palídromo')
   } else {
-    alert('Sinto muito!, o nome que você digitou não é um palíndromo')}
-
+    alert('Sinto muito!, o nome que você digitou não é um palíndromo')
+  }
 }
 
-verificaPalindromo()
+verificaPalindromo1()*/
 
-// Solução 2 
+// Solução 2
+
+function verificaPalindromo3(string) {
+  if (!string) return 'string inexistente'
+
+  for (let i = 0; i < string.length / 2; i++) {
+    if (string[i] !== string[string.length - 1 - i]) {
+      return false
+    }
+  }
+
+  return true
+}
+
+console.log(verificaPalindromo3('ovo'))
