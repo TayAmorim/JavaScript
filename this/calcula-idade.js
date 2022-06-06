beaf4fb73bb079c037aba59a08e50a785d31e407
TayAmorim <amorim.tayanna@gmail.com>
2022-06-06 14:23:09 -0300

    add novo exemplo

--- this/calcula-idade.js
+++ this/calcula-idade.js
@@ -1,4 +1,4 @@
-function calculaIdade(anos) {
+/*function calculaIdade(anos) {
   return`Daqui a ${anos} anos, ${this.nome} terá ${this.idade + anos} anos de idade.`;
 }
 
@@ -18,4 +18,32 @@ const animal = {
   raca:"Pug"
 }
 
-console.log(calculaIdade.call(pessoa1, 30))
+console.log(calculaIdade.call(pessoa1, 30))*/
+
+function calcularCompras (valor) {
+  return `Olá ${this.nome}, o troco da sua compra foi de ${valor - this.money} na ${this.dia}`
+}
+
+const pessoa1 = {
+  nome:"Joana",
+  money: 25,
+  dia:"Segunda"
+
+}
+
+const pessoa2 = {
+  nome:"Fernanda",
+  money: 50,
+  dia:"Quarta"
+
+}
+
+const pessoa3 = {
+  nome:"José",
+  money: 80,
+  dia:"sexta"
+
+}
+
+console.log(calcularCompras.call(pessoa1, 60))
+
