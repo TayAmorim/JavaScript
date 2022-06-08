f8042765ea725a957be1638d0e83dce409723d81
TayAmorim <amorim.tayanna@gmail.com>
2022-06-08 14:43:38 -0300

    Finalização do arquivo js

--- orientacao-a-objeto/scripts.js
+++ orientacao-a-objeto/scripts.js
@@ -9,5 +9,58 @@ class ContaBancaria {
   get saldo() {
     return this._saldo;
   }
-  
+
+  set saldo(valor) {
+    this._saldo = valor;
+  }
+
+  sacar(valor) {
+    if(valor > this._saldo) {
+      return 'Operação negada'
+    }
+    this._saldo = this._saldo - valor
+    return this._saldo
+  }
+
+  depositar(valor) {
+    this._saldo = this._saldo + valor
+    return this._saldo
+  }
+}
+
+class ContaCorrente {
+  constructor(agencia, numero, saldo, cartaoCredito) {
+    super(agencia, numero, saldo)
+    this.tipo = 'Corrente'
+    this._cartaoCredito = cartaoCredito
+  }
+
+  get cartaoCredito() {
+    return this._cartaoCredito;
+  }
+
+  set cartaoCredito(valor) {
+    this._cartaoCredito = valor
+  }
+}
+
+class ContaPoupanca {
+  constructor(agencia, numero, saldo) {
+    super(agencia, numero, saldo)
+    this.tipo = 'Poupança'
+  }
+}
+
+class ContaUniversitaria {
+  constructor(agencia, numero, saldo) {
+    super(agencia, numero, saldo)
+    this.tipo = 'Universitária'
+  }
+
+  sacar(valor) {
+    if(valor > 500) {
+      return 'Operação negada!'
+    }
+    this._saldo = this._saldo - valor
+  }
 }
