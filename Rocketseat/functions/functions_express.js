258bae55c6fb8a491d49f96a0cfe2320db8f1b52
TayAmorim <amorim.tayanna@gmail.com>
2022-06-09 14:46:58 -0300

    function scope

---
+++ Rocketseat/functions/functions_express.js
@@ -0,0 +1,26 @@
+// function express
+// function anonymous
+
+// parameters
+
+/*
+const sum = function(number1, number2) {
+  total = number1 + number2
+  return total
+}
+
+
+let number1 = 34
+let number2 = 25
+sum(number1, number2) 
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`a soma é ${total}`) */
+
+function fazerSuco(fruta1, fruta2){
+  return fruta1 + fruta2
+}
+
+const copo = fazerSuco('banana', 'maça')
+console.log(copo)
