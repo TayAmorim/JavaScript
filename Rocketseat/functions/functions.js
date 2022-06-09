686c2e22964c4571aadc82b740753ccf97f5cea2
TayAmorim <amorim.tayanna@gmail.com>
2022-06-09 14:38:59 -0300

    add de function_1

--- Rocketseat/functions/functions.js
+++ Rocketseat/functions/functions.js
@@ -1,14 +1,26 @@
-// criar um aplicativo de frases motivacionais
+// function express
+// function anonymous
 
-// declaration - declaração da função
-// function statement
+// parameters
 
-function createPhrases () {
-  console.log('A persistência é o caminho do êxito.')
-  console.log('As pessoas costumam dizer que a motivação não dura sempre. Bem, nem o efeito do banho, por isso recomenda-se diariamente.')
+/*
+const sum = function(number1, number2) {
+  total = number1 + number2
+  return total
 }
 
-// execute, run, call, Invoke
-createPhrases()
 
-console.log('fim do programa')
+let number1 = 34
+let number2 = 25
+sum(number1, number2) 
+
+console.log(`O número 1 é ${number1}`)
+console.log(`O número 2 é ${number2}`)
+console.log(`a soma é ${total}`) */
+
+function fazerSuco(fruta1, fruta2){
+  return fruta1 + fruta2
+}
+
+const copo = fazerSuco('banana', 'maça')
+console.log(copo)
