a7973ae709ac99f07b01db8a1dc82de385e4338b
TayAmorim <amorim.tayanna@gmail.com>
2022-06-09 13:39:30 -0300

    add exercicio relebrando

---
+++ Rocketseat/praticando-e-avancado/script.js
@@ -0,0 +1,74 @@
+// 1. Declare uma variável de nome weight
+//let weight
+
+// 2. Que tipo de dado é a variável acima?
+//console.log(typeof weight)
+
+/* 3. Declare uma varíavel e atribua valores para cada um dos dados:
+    * name: String
+    * age: Number (integer)
+    * stars: Number (float)
+    * isSubscribed: Boolean
+  
+  let name = 'Tayanna'
+  let age = 27
+  let stars = 1.52
+  let isSubscribed = true
+*/
+
+/* 4. A variável student abaixo é de que tipo de dados? 
+  let student = {};
+  Object
+
+  4.1 Atribua a ela as mesma propriedades e valores do exercício 3.
+    let student = {
+    name: 'tayanna',
+    age: 27,
+    weight: 58,
+    isSubscribed: true
+  }
+
+  4.2 Mostre no console a seguinte mensagem:
+
+    <name> de idade <age> pesa <weight> kg.
+
+    Atenção, substitua <name> <age> e <weight> pelos valores de cada propriedade do objeto
+
+    console.log(` ${student.name} de idade ${student.age} pesa ${student.weight} Kg.`)
+*/
+
+/* 5. Declare uma variável do tipo Array, de nome students e atribua a ela nenhum valor, ou seja, somente o Array vazio
+  let student = []
+*/
+
+/* 6. Reatribua valor para a variável acima, colocando dentro dela o objeto student da questão 4. (não copiar e colar o objeto, mas usar o objeto criado e inserir ele no Array)
+  students = [
+  student
+]
+*/
+
+/* 7. Coloque no console o valor da posição zero do Array acima
+console.log(students[0])
+*/
+
+/* 8. Crie um novo student e coloque na posição 1 do Array students
+  let student = {
+  name: 'tayanna',
+  age: 27,
+  weight: 58,
+  isSubscribed: true
+}
+
+const John = {
+  name: 'Jhon',
+  age: 28,
+  weight: 67,
+  isSubscribed: true
+}
+
+let students = [student]
+
+students[1] = John
+
+console.log(students)
+ */
