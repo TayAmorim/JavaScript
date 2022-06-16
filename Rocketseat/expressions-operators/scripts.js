af6213f0d8b6252cd771ea5b58633b497f804c42
TayAmorim <amorim.tayanna@gmail.com>
2022-06-16 13:12:57 -0300

    add da pasta expressions

---
+++ Rocketseat/expressions-operators/scripts.js
@@ -0,0 +1,72 @@
+//Expressão e Operadores
+
+/** 
+ * Expressions
+ * Operators
+    - Binary
+       let number = 1
+       console.log(number + 1)
+    - Unary
+      let number = 1
+      console.log(++number)
+    - Ternary
+      let number = 1
+      console.log(false ? 'alo' : 'nada')
+ */
+
+/**
+ * New
+ *
+ * Left-hand-side expression
+ * Criar um novo objeto
+    let name = new String('Tayanna')
+    name.surName = 'Amorim'
+    let age = new Number(23)
+    console.log(name, name.surName)
+ */
+
+/**
+ * Operadores unários
+ * 
+ * typeof
+ * delet
+    const person = {
+    name: 'Tayanna',
+    age: 27
+}
+
+    delete person.age
+
+    console.log(person)
+ */
+
+/**
+ * Operadores Aritméticos
+ *
+ * multiplicação *
+    console.log(3.2 * 5.5)
+  
+ * divisão /
+    console.log(12 / 3)
+ 
+ * soma
+    console.log(25 + 3)
+  
+ * subtração
+    console.log(45 - 5)
+ 
+ * resto da divisão
+ * 
+ * 
+ * incremento
+ * 
+ * 
+ * decremento
+ *
+ * 
+ * exponencial
+ * 
+ *  
+ */
+
+console.log(45 - 5)
